d7bc1e6ffe71026783a2c7d025ffa322fb5f853e
hari208 <harish.barigi@gmail.com>
2021-04-19 13:20:00 +0530

    temp1 added

--- temp.c
+++ temp.c
@@ -17,3 +17,23 @@ void main()
 	write(clifd,buf,strlen(buf));
 	printf("clint send:%s\n",buf);
 }
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+void main()
+{
+	int srvfd;
+	int ret,ret1;
+	char buf[15];
+	ret1=mkfifo("srvfifo",0666);
+//	printf("ret1=%d\n",ret1);
+	srvfd=open("srvfifo",O_RDONLY);
+//	printf("open ret=%d\n",srvfd);
+	ret=read(srvfd,buf,15);
+	buf[ret]='\0';
+	printf("srever receives:%s\n",buf);
+}
